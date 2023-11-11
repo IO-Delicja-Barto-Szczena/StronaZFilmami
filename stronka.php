22d787db74f2a254c79431a0fe2386921ebdfbe3
Sp3EdQQ <bartekjanik45@gmail.com>
2023-11-11 01:41:55 +0100

    zaczynamy

--- stronka.php
+++ stronka.php
@@ -3,9 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Strona</title>
+    <title>Strona IO</title>
+    <link rel="stylesheet" href="kod_css.css">
 </head>
-<body>\
-    Strona z filmami1
+<body>
+
+    Strona z filmami
+
 </body>
 </html>
