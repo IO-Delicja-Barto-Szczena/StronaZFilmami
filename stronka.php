b7dc65ddaabf52a3f473e79bc08017d033d04698
Sp3EdQQ <bartekjanik45@gmail.com>
2023-11-18 02:35:47 +0100

    carousel, navbar working

--- stronka.php
+++ stronka.php
@@ -8,13 +8,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Strona IO</title>
+    
     <!-- Bootstrap CSS -->  
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
-    <!-- Bootstrap JS, jQuery, Popper.js -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    
+    <!--4.0--><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    
 
 
 
@@ -22,7 +20,6 @@
 
     <link rel="stylesheet" href="main.css">
 
-    <link rel="stylesheet" href="css/carouselmain.css">
 
 
 
@@ -34,29 +31,33 @@
 </head>
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light"><!--to ten jebitny pasek na gorze -->
-    
-    <a class="navbar-brand" href="#">Logo</a> <!-- przekierowanie na glowna strone, tu ma byc nasze logo -->
-    
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">About</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Services</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Contact</a>
-            </li>
-        </ul>
-    </div>
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+
+<a class="navbar-brand" href="#">Logo</a>
+
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+<span class="navbar-toggler-icon"></span>
+</button>
+
+<div class="collapse navbar-collapse" id="navbarNav">
+
+<ul class="navbar-nav ml-auto">
+    <li class="nav-item active">
+    <a class="nav-link" href="#">Strona główna <span class="sr-only">(current)</span></a>
+    </li>
+    <li class="nav-item">
+    <a class="nav-link" href="#">O nas</a>
+    </li>
+    <li class="nav-item">
+    <a class="nav-link" href="#">Usługi</a>
+    </li>
+    <li class="nav-item">
+    <a class="nav-link" href="#">Kontakt</a>
+    </li>
+</ul>
+
+</div>
+
 </nav>
 
 
@@ -64,36 +65,43 @@
 Strona z filmami
 
 
-<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
 
-    <div class="carousel-inner">
-        <div class="carousel-item active">
-            <img src="./pics/carouselmainfirst/carpic1.jpg" class="d-block w-100" alt="...">
-        </div>
-        <div class="carousel-item">
-            <img src="./pics/carouselmainfirst/carpic2.jpg" class="d-block w-100" alt="...">
-        </div>
-        <div class="carousel-item">
-            <img src="./pics/carouselmainfirst/carpic3.jpg" class="d-block w-100" alt="...">
-        </div>
-    </div>
 
-    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
+
+
+<!-- jebana karuzela -->
+
+<div class="col-md-8"> 
+    <div id="carouselExample" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+            <img src="./pics/carouselmainfirst/carpic1.jpg" class="d-block w-100" alt="Image 1">
+            </div>
+            <div class="carousel-item">
+            <img src="./pics/carouselmainfirst/carpic2.jpg" class="d-block w-100" alt="Image 2">
+            </div>
+            <div class="carousel-item">
+            <img src="./pics/carouselmainfirst/carpic3.jpg" class="d-block w-100" alt="Image 3">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExample" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
         </a>
-        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-    </a>
+        <a class="carousel-control-next" href="#carouselExample" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+</div>
 
-</div>  
+
+
+
+
 
 
 <?php
-
-
-
 
 
 //! nie tykaj
@@ -117,6 +125,15 @@ if (!$conn) {
 // Close connection
 mysqli_close($conn);
 ?>
+
+
+
+
+
+<!-- TO MUSI BYC NA KONCU-->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 </html>
 
